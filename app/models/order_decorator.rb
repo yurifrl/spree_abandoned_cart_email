eebba58a18b504c156fe65092e3913de00ada1c1
Yurifrl <yurifrl@hotmail.com>
2014-06-06 12:29:18 -0300

    update

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,26 +1,17 @@
 Spree::Order.class_eval do
-
-  preference :abandedon_email_timeframe, 5.minutes
+  #preference :abandedon_email_timeframe, 5.minutes
 
   def self.email_eligible_abandoned_email_orders
     eligible_abandoned_email_orders.each { |o| o.send_abandoned_email }
   end
 
   def self.eligible_abandoned_email_orders
-    where("state != ?
-          AND (payment_state IS NULL OR payment_state != ?)
-          AND email is NOT NULL
-          AND abandoned_email_sent_at IS NULL
-          AND created_at < ?",
-          "complete",
-          "paid",
-          (Time.zone.now - Spree::AbandonedCartEmailConfig::Config.email_timeframe))
+    where("state != ? AND (payment_state IS NULL OR payment_state != ?) AND email is NOT NULL AND abandoned_email_sent_at IS NULL AND created_at < ?", "complete", "paid", (Time.zone.now - Spree::AbandonedCartEmailConfig::Config.email_timeframe))
   end
 
   def send_abandoned_email
-# Don't send anything if the order has no line items.
+    # Don't send anything if the order has no line items.
     return if line_items.empty?
-
     Spree::AbandonedCartMailer.abandoned_email(self).deliver
     mark_abandoned_email_as_sent
   end
@@ -30,5 +21,4 @@ Spree::Order.class_eval do
   def mark_abandoned_email_as_sent
     update_attribute :abandoned_email_sent_at, Time.zone.now
   end
-
 end
